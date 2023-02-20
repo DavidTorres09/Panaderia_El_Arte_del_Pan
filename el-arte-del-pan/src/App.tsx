bcbbde946b0d55122a46252fd6b1e1cbeed60a61
JoZhang06 <joe.chang.1996@hotmail.com>
2023-02-20 16:18:38 -0600

    Add new compnent folder and SideBar component

--- el-arte-del-pan/src/App.tsx
+++ el-arte-del-pan/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import Griddb from './Grid_DB/griddb';
+import Griddb from './Components/Grid_DB/griddb';
+import SideBar from './Components/SideBar/SideBar';
 
 const App = () => {
   return (
     <div>
       <Griddb />
+      <SideBar/>
     </div>
   );
 };
